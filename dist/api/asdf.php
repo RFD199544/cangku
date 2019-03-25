63bcede6e28d439036b136d5e1f193c8a1b3b17f
rong <847841499@qq.com>
2019-03-25 19:33:47 +0800

    1

--- dist/api/asdf.php
+++
@@ -1,8 +0,0 @@
-if($select=='moren'){//搜索默认排序
-            $sql="SELECT * FROM goodslist WHERE name LIKE '%$val%' OR bar LIKE '%$val%' OR brand LIKE '%$val%' LIMIT $page1,$page2";
-        }else 
-
-
-        if($select=='moren'){//搜索默认排序
-            $sql="SELECT * FROM goodslist LIMIT $page1,$page2";
-        }else 
